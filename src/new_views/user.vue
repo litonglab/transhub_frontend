b5a237cc38af2b4ade4c1f660a96abda8549ac67
ggbondzhu <878038720@qq.com>
2025-06-11 11:50:05 +0800

    fix: user.vue: 修复重置按钮不生效

--- src/new_views/user.vue
+++ src/new_views/user.vue
@@ -15,23 +15,41 @@
                 </v-col>
                 <v-col cols="12" md="3">
                   <v-list-item-title>真实姓名:</v-list-item-title>
-                  <v-list-item-subtitle>{{ real_info.real_name }}</v-list-item-subtitle>
+                  <v-list-item-subtitle
+                  >{{ real_info.real_name }}
+                  </v-list-item-subtitle>
                 </v-col>
                 <v-col cols="12" md="3">
                   <v-list-item-title>学号:</v-list-item-title>
-                  <v-list-item-subtitle>{{ real_info.sno }}</v-list-item-subtitle>
+                  <v-list-item-subtitle
+                  >{{ real_info.sno }}
+                  </v-list-item-subtitle>
                 </v-col>
                 <v-col cols="12" md="3">
                   <v-list-item-title>课程:</v-list-item-title>
-                  <v-list-item-subtitle>{{ real_info.cname }}</v-list-item-subtitle>
+                  <v-list-item-subtitle
+                  >{{ real_info.cname }}
+                  </v-list-item-subtitle>
                 </v-col>
               </v-row>
             </v-col>
           </v-row>
           <v-row class="mt-4">
             <v-col cols="12" class="text-center">
-              <v-btn color="red-darken-4" outlined @click="pwd_visible = true" class="mx-2">修改密码</v-btn>
-              <v-btn color="red-darken-4" outlined @click="dialogVisible_3 = true" class="mx-2">修改信息</v-btn>
+              <v-btn
+                color="red-darken-4"
+                outlined
+                @click="pwd_visible = true"
+                class="mx-2"
+              >修改密码
+              </v-btn>
+              <v-btn
+                color="red-darken-4"
+                outlined
+                @click="dialogVisible_3 = true"
+                class="mx-2"
+              >修改信息
+              </v-btn>
             </v-col>
           </v-row>
         </v-sheet>
@@ -44,14 +62,26 @@
         <v-card-title>修改密码</v-card-title>
         <v-card-text>
           <v-form ref="pwdFormRef">
-            <v-text-field label="旧密码" v-model="formLabelAlign.oldpwd" prepend-icon="mdi-lock" type="password"
-                          :rules="rules.oldpwd"
+            <v-text-field
+              label="旧密码"
+              v-model="formLabelAlign.oldpwd"
+              prepend-icon="mdi-lock"
+              type="password"
+              :rules="rules.oldpwd"
             ></v-text-field>
-            <v-text-field label="新密码" v-model="formLabelAlign.newpwd" prepend-icon="mdi-lock" type="password"
-                          :rules="rules.newpwd"
+            <v-text-field
+              label="新密码"
+              v-model="formLabelAlign.newpwd"
+              prepend-icon="mdi-lock"
+              type="password"
+              :rules="rules.newpwd"
             ></v-text-field>
-            <v-text-field label="确认密码" v-model="formLabelAlign.confirmpwd" prepend-icon="mdi-lock" type="password"
-                          :rules="rules.confirmpwd"
+            <v-text-field
+              label="确认密码"
+              v-model="formLabelAlign.confirmpwd"
+              prepend-icon="mdi-lock"
+              type="password"
+              :rules="rules.confirmpwd"
             ></v-text-field>
           </v-form>
         </v-card-text>
@@ -111,33 +141,28 @@ const formLabelAlign = ref({
   confirmpwd: "",
 });
 const rules = {
-  oldpwd: [
-    {required: true, message: '请输入旧密码', trigger: ['blur', 'input']}
-  ],
-  newpwd: [
-    {required: true, message: '请输入新密码', trigger: ['blur', 'input']}
-  ],
+  oldpwd: [(v) => !!v || "请输入旧密码"],
+  newpwd: [(v) => !!v || "请输入新密码"],
   confirmpwd: [
-    (value) => !!value || '请确认新密码', // 必填规则
-    (value) => value === formLabelAlign.value.newpwd || '两次输入不一致' // 一致性规则
-  ]
+    (v) => !!v || "请确认新密码",
+    (v) => v === formLabelAlign.value.newpwd || "两次输入不一致",
+  ],
 };
 
 const info_rules = {
-  real_name: [{required: true, message: "请输入真实姓名", trigger: "blur"}],
-  sno: [{required: true, message: "请输入学号", trigger: "blur"}],
-  myclass: [{required: true, message: "请选择课程", trigger: "change"}],
+  real_name: [(v) => !!v || "请输入真实姓名"],
+  sno: [(v) => !!v || "请输入学号"],
+  myclass: [(v) => !!v || "请选择课程"],
 };
 
 const realInfoRef = ref(null);
 const pwdFormRef = ref(null); // 声明表单引用
 
 const change_pwd = async () => {
-
   const {valid} = await pwdFormRef.value.validate({force: true});
   if (!valid) return; // 验证失败则阻止提交
-  console.log(pwdFormRef.value)
-  console.log('change_pwd：', valid);
+  console.log(pwdFormRef.value);
+  console.log("change_pwd：", valid);
   try {
     await request(APIS.changepwd, {
       body: JSON.stringify({
@@ -149,7 +174,6 @@ const change_pwd = async () => {
     pwd_visible.value = false;
   } catch (error) {
   }
-
 };
 
 const update_info = async () => {
@@ -165,16 +189,29 @@ const update_info = async () => {
   }
 };
 
-const resetForm = (formName) => {
-  formRef.value.resetFields();
+const resetForm = () => {
+  formLabelAlign.value = {
+    oldpwd: "",
+    newpwd: "",
+    confirmpwd: "",
+  };
+  pwdFormRef.value?.reset();
+};
+
+const resetForm2 = () => {
+  real_info.value = {
+    real_name: "",
+    sno: "",
+    cname: "",
+  };
+  realInfoRef.value?.reset();
 };
 
 async function getRealInfo() {
   try {
-    const result = await request(APIS.get_info,
-      {
-        method: "GET"
-      },);
+    const result = await request(APIS.get_info, {
+      method: "GET",
+    });
     real_info.value.real_name = result.real_info.real_name;
     real_info.value.sno = result.real_info.sno;
     real_info.value.cname = result.real_info.cname;
@@ -185,13 +222,10 @@ async function getRealInfo() {
 onMounted(async () => {
   getRealInfo();
   try {
-    const result = await request(
-      APIS.get_pantheon,
-      {
-        method: "GET"
-      },
-    );
-    pantheons.value = result['pantheon'] || [];
+    const result = await request(APIS.get_pantheon, {
+      method: "GET",
+    });
+    pantheons.value = result["pantheon"] || [];
   } catch (error) {
     pantheons.value = [];
   }
